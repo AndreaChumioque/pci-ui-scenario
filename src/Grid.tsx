0d1d779f5888acec1c39d3c4a1b8c72f5ad4be9c
AndreaChumioque <aichumioque@gmail.com>
2023-01-18 13:49:48 -0500

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,17 +4,102 @@ import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+const defaultColDef: ColDef<{}> = {
+  sortable: true,
+};
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  {
+    field: "designation",
+    headerName: "Designation",
+    filter: 'agTextColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    filter: 'agTextColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "h_mag",
+    headerName: "H (mag)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "moid_au",
+    headerName: "MOID (au)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "q_au_1",
+    headerName: "q (au)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "q_au_2",
+    headerName: "Q (au)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "period_yr",
+    headerName: "Period (yr)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    filter: 'agNumberColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "pha",
+    headerName: "Potentially Hazardous",
+    filter: 'agTextColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
+  {
+    field: "orbit_class",
+    headerName: "Orbit Class",
+    enableRowGroup: true,
+    filter: 'agTextColumnFilter',
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
+  },
 ];
 
 const NeoGrid = (): JSX.Element => {
@@ -23,6 +108,7 @@ const NeoGrid = (): JSX.Element => {
       <h1>Near-Earth Object Overview</h1>
       <AgGridReact
         rowData={data}
+        defaultColDef={defaultColDef}
         columnDefs={columnDefs}
         rowGroupPanelShow={'always'}
       />
